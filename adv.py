bfc2a5946947df1052e9903cdd5de787ae8ffe1a
ryanp <ryanpanimation@gmail.com>
2020-11-23 01:09:55 -0600

    working pathing

--- adv.py
+++ adv.py
@@ -4,6 +4,7 @@ from world import World
 
 import random
 from ast import literal_eval
+from collections import deque, defaultdict
 
 # Load world
 world = World()
@@ -12,12 +13,12 @@ world = World()
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
+map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -27,8 +28,67 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
 
+# ! do a bft so we try to get shortest path?
+
+traversal_path = []
+visited_rooms = set()
+room_list = deque()
+room_list.append(player.current_room.id)
+
+while len(list(visited_rooms)) < len(list(room_graph)):
+    # while len(room_queue) > 0:
+    curr = room_list[-1]
+
+    visited_rooms.add(curr)
+
+    room_queue = deque()
+
+    neighbors = room_graph[curr][1]
+    for v in neighbors.values():
+        if v not in visited_rooms:
+            room_queue.append(v)
+
+    if len(room_queue) > 0:
+        new_room = room_queue.popleft()
+        room_list.append(new_room)
+    else:
+        room_list.pop()
+
+    for k, v in neighbors.items():
+        if v == room_list[-1]:
+            traversal_path.append(k)
+
+"""
+
+while len(visited_rooms) < len(room_graph):
+    room_exits = player.current_room.get_exits()
+    print(room_exits)
+    exit_options = []
+    for e in room_exits:
+        if player.current_room.get_room_in_direction(e) not in visited_rooms:
+            exit_options.append(e)
+
+    visited_rooms.add(player.current_room)
+
+    if len(exit_options) > 0:
+        move_dir = random.choice(exit_options)
+        # print(move_dir)
+        room_queue.append(move_dir)
+        player.travel(move_dir)
+        traversal_path.append(move_dir)
+    else:
+        move_dir = room_queue.popleft()
+        player.travel(move_dir)
+        traversal_path.append(move_dir)
+        print("end of path")
+        
+"""
+
+print("visited_rooms")
+print(visited_rooms)
+print("path")
+print(traversal_path)
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -41,22 +101,22 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited"
+    )
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
-
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
